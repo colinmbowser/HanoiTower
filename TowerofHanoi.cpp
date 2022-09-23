cb023ce100e304ddbd21e24e6530b90f36be7e4c
Colin Bowser <cbowser@byui.edu>
2022-09-23 00:01:53 -0700

    wrote code to get integer input.

--- TowerofHanoi.cpp
+++ TowerofHanoi.cpp
@@ -2,13 +2,32 @@
 
 using namespace std;
 
+int getNum()
+{
+    int blockInputAmount = 0;
+    cout << "Please enter the amount of blocks you want Tower of Hanoi to have (up to 10): ";
+    cin >> blockInputAmount;
+    while (blockInputAmount < 1 || blockInputAmount > 10)
+    {
+        cin.clear();
+        cin.ignore(256, '\n');
+        cout << "please enter a number between 1 and 10: ";
+        cin >> blockInputAmount;
+    }
+    return blockInputAmount;
+}
+
+void TowerofHanoi()
+{
+    int blockAmount = getNum();
+
+
+    cout << blockAmount;
+
+}
+
 int main()
 {
-    int ten = 10;
-    ten = 5;
-
-
-    cout << ten << endl;
-    //cout << "Hello World." << endl;
+    TowerofHanoi();
     return 0;
 }
